369bb38fa10d4f0666fe19452160d3eb6e620659
NellShark <nellshark1@gmail.com>
2022-06-07 12:44:34 +0600

    Initial commit

---
+++ src/main/java/com/nellshark/springbootblog/SpringbootblogApplication.java
@@ -0,0 +1,13 @@
+package com.nellshark.springbootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nellshark/springbootblog/SpringbootblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nellshark.springbootblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
