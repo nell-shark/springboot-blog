eec1ffef6d10c3716e1e9e0b1bdb66a28f854d32
NellShark <nellshark1@gmail.com>
2022-10-29 13:34:52 +0600

    Minify html

---
+++ src/main/java/com/nellshark/springbootblog/filter/HtmlFilter.java
@@ -0,0 +1,42 @@
+package com.nellshark.springbootblog.filter;
+
+import com.googlecode.htmlcompressor.compressor.HtmlCompressor;
+import com.nellshark.springbootblog.wrapper.CharResponseWrapper;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class HtmlFilter implements Filter {
+    private FilterConfig config;
+
+    public void init(FilterConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        ServletResponse newResponse = servletResponse;
+
+        if (servletRequest instanceof HttpServletRequest) {
+            newResponse = new CharResponseWrapper((HttpServletResponse) servletResponse);
+        }
+
+        filterChain.doFilter(servletRequest, newResponse);
+
+        if (newResponse instanceof CharResponseWrapper) {
+            String text = newResponse.toString();
+            if (text != null) {
+                HtmlCompressor htmlCompressor = new HtmlCompressor();
+                servletResponse.getWriter().write(htmlCompressor.compress(text));
+            }
+        }
+    }
+
+    public FilterConfig getConfig() {
+        return config;
+    }
+}
